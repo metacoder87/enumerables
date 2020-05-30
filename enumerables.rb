90c30dbbd67a1e816da8f66f0e47ddcea3500b6b
metacoder87 <gary.miller@gmx.com>
2020-05-30 12:15:43 -0400

    Adds in the my_each method

--- enumerables.rb
+++ enumerables.rb
@@ -5,3 +5,16 @@ require "byebug"
 puts "My monkey patch for the Array Class"
 
 class Array
+
+# My Each
+# Extend the Array class to include a method named my_each that takes a block, 
+# calls the block on every element of the array, and returns the original array. 
+# Do not use Enumerable's each method.
+
+    def my_each(&prc)
+        self.to_a.map do |ele|
+            prc.call(ele)
+        end
+        return self
+    end
+
