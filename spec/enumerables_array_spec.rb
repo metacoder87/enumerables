62f2d52d6af96491b69d6684321f01b4f79f0b15
metacoder87 <gary.miller@gmx.com>
2020-05-30 15:57:54 -0400

    Adds in the tests for substrings and subwords

--- spec/enumerables_array_spec.rb
+++ spec/enumerables_array_spec.rb
@@ -10,3 +10,34 @@ describe "#factors" do
     expect(factors(13)).to eq([1, 13])
   end
 end
+
+describe "#substrings" do
+  it "should take a `String`" do
+    expect { substrings("string") }.not_to raise_error
+  end
+
+  it "should return an array containing each of its substrings" do
+    expect(substrings("cat")).to eq(["c", "ca", "cat", "a", "at", "t"])
+  end
+end
+
+describe "#subwords" do
+  it "can find a simple word" do
+    words = subwords("asdfcatqwer", ["cat", "car"])
+    expect(words).to eq(["cat"])
+  end
+
+  it "doesn't find spurious words" do
+    words = subwords("batcabtarbrat", ["cat", "car"])
+    expect(words).to be_empty
+  end
+
+  it "can find words within words" do
+  #note that the method should NOT return duplicate words
+    dictionary = ["bears", "ear", "a", "army"]
+    words = subwords("erbearsweatmyajs", dictionary)
+
+    expect(words).to eq(["bears", "ear", "a"])
+  end
+end
+
